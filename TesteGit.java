366426cae6d8d862d3973b4b6fb59b3a80f35281
Lucas Passarela <lucaspassarela@yahoo.com.br>
2014-12-09 14:14:08 -0200

    Teste 3

--- TesteGit.java
+++ TesteGit.java
@@ -1,6 +1,6 @@
 
 public class TesteGit {
-	// Teste 2
+	// Teste 3
 	public static void main (String[] args) {
 		System.out.println("Exemplo de teste de uso do git");
 		System.out.println("Trabalhando com branches");
